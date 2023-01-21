e0415e67134a69a42b655dc02bd5a780e8c0c6c7
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-01-20 18:30:26 -0800

    feat: added create product button in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
     <div className="main-footer">
+    <
+      div>Click to add product</div>
+        <Link to='/create'>
+                <div className="create-link-container"><img className="create-link" src="https://cdn.pixabay.com/photo/2017/01/10/23/01/icon-1970474_1280.png"></img></div>
+            </Link>
+
       <div className="container">
         <div className="row">
           {/* Column1 */}
@@ -27,7 +35,7 @@ const Footer = () => {
           <div className="column">
             <h4>COLUMN 3</h4>
             <ul className="list-unstyled">
-            <li>line 1</li>
+              <li>line 1</li>
               <li>line 2</li>
               <li>line 3</li>
             </ul>
@@ -36,7 +44,7 @@ const Footer = () => {
           <div className="column">
             <h4>COLUMN 4</h4>
             <ul className="list-unstyled">
-            <li>line 1</li>
+              <li>line 1</li>
               <li>line 2</li>
               <li>line 3</li>
             </ul>
@@ -45,6 +53,6 @@ const Footer = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,6 @@ function Header() {
             <Link to='/'>
                 <div className="app-title"><img className="stock-kawaii-app-icon" src="https://img.freepik.com/premium-vector/cute-shiba-inu-snowboarding-cartoon-vector-icon-illustration_480044-354.jpg"></img><p className="app-name">Snowpad</p></div>
             </Link>
-            <Link to='/create'>
-                <div className="create-link-container"><img className="create-link" src="https://cdn.pixabay.com/photo/2017/01/10/23/01/icon-1970474_1280.png"></img></div>
-            </Link>
         </div>
     );
 }
